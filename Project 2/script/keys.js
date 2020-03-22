18a2d136f8f2d532e56f753670eb4e2f4c7bc42d
wvond <von16004@byui.edu>
2020-03-21 21:21:11 -0600

    add Great Schools API key

---
+++ Project 2/script/keys.js
@@ -0,0 +1,8 @@
+/**************************************************************
+ * The below API key is from the website that owns the data on 
+ * the Great Schools API website.  The key will have an 
+ * expiration date of May 14th 2020.
+ **************************************************************/
+export const key = '72428a4cbc31424b958fd7a9100a72b5';
+
+// api key website https://www.greatschools.org/api
